e2fc27cf22d557dc36e67bfeade75de3006c39c2
Lucas Novaes <139508004+lucassyllva@users.noreply.github.com>
2024-05-06 21:36:36 -0300

    Update CardapioPizzas.java

--- CardapioPizzas.java
+++ CardapioPizzas.java
@@ -4,7 +4,8 @@ public class CardapioPizzas {
     int valorMedia = 35;
     int valorBroto = 25;
     int medida;
-    
+    int refriG =10;
+    int refriLata =5;
     
     void TAMANHOSpizzas (){
         //System.out.println("MUSSARELA - TAMANHO FAMILIA");
@@ -35,18 +36,36 @@ public class CardapioPizzas {
     }
     void sabores(){
         System.out.println("MUSSARELA - " + medida+",00 R$");
-                System.out.println("CALABRESA - " + medida+",00 R$");
-                System.out.println("MARGUERITA - " + medida+",00 R$");
-                System.out.println("4 QUEIJOS - " + medida+",00 R$");
-                System.out.println("TOSCANA - " + medida+",00 R$");
-                System.out.println("ALHO FRITO - " + medida+",00 R$");
-                System.out.println("PORTUGUESA - " + medida+",00 R$");
-                System.out.println("FRANGO COM CATUPIRY - " + medida+",00 R$");
-                System.out.println("NAPOLITANA - " + medida+",00 R$");
-                System.out.println("BROCOLIS - " + medida+",00 R$");
-                System.out.println("PALMITO - " + medida+",00 R$");
-                System.out.println("ATUM - " + medida+",00 R$");
-                System.out.println("BOLONHESA - " + medida+",00 R$");
+                System.out.println("01- CALABRESA - " + medida+",00 R$");
+                System.out.println("02- MARGUERITA - " + medida+",00 R$");
+                System.out.println("03- 4 QUEIJOS - " + medida+",00 R$");
+                System.out.println("04- TOSCANA - " + medida+",00 R$");
+                System.out.println("05- ALHO FRITO - " + medida+",00 R$");
+                System.out.println("06- PORTUGUESA - " + medida+",00 R$");
+                System.out.println("07- FRANGO COM CATUPIRY - " + medida+",00 R$");
+                System.out.println("08- NAPOLITANA - " + medida+",00 R$");
+                System.out.println("09- BROCOLIS - " + medida+",00 R$");
+                System.out.println("10- PALMITO - " + medida+",00 R$");
+                System.out.println("11- ATUM - " + medida+",00 R$");
+                System.out.println("12- BOLONHESA - " + medida+",00 R$");
 
     }
+
+    void PizzaDoce(){
+        System.out.println("01- CHOCOLATE COM MORANGO "+valorBroto+",00R$");
+        System.out.println("02- CHURROS "+valorBroto+",00R$");
+        System.out.println("03- CHOCOLATE "+valorBroto+",00R$");
+        System.out.println("04- LEITE NINHO COM NUTELLA "+valorBroto+",00R$");
+
+    }
+
+    void Refrigerantes(){
+        System.out.println("01- COCA-COLA 2L "+refriG+",00R$");
+        System.out.println("02- GUARRANA 1,5L "+refriG+",00R$");
+        System.out.println("03- FANTA LARANJA 2L "+refriG+",00R$");
+        System.out.println("04- COCA-COLA LATA 350ML "+refriLata+",00R$");
+        System.out.println("05- GUARRANA LATA 350ML"+refriLata+",00R$");
+        System.out.println("06- SPLITE LATA 350ML"+refriLata+",00R$");
+    }
+
 }
